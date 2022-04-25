2028adf9966d17e7da9ccd3c9cdf6b7f20631acb
Joe Torreggiani <joetorreggiani@gmail.com>
2022-04-24 23:11:15 -0700

    Initial react app.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Data Umbrella Community Events</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
