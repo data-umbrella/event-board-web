a2f55e555bfc6ea6e5639e1f7f9c7271f875ddca
Joe Torreggiani <joetorreggiani@gmail.com>
2022-06-20 15:39:25 -0700

    Add basic testing.

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -2,6 +2,12 @@ function LoginPage() {
   return (
     <div>
       <h2>Login</h2>
+
+      <label>Email</label>
+      <input type="text"></input>
+
+      <label>Password</label>
+      <input type="text"></input>
     </div>
   )
 }
