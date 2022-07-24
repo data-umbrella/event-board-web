a59d804878c43b7fbe9fae462fff57cb8d1c10b7
Joe Torreggiani <joetorreggiani@gmail.com>
2022-07-24 10:54:08 -0700

    Clean up.

--- src/components/pages/SponsorsPage.jsx
+++ src/components/pages/SponsorsPage.jsx
@@ -1,6 +1,6 @@
 function SponsorsPage() {
   return (
-    <div>
+    <div className="container mx-auto p-10">
       <h1>Sponsors</h1>
 
       <p>If you would like to sponsor this Event Board, please submit a form here.</p>
