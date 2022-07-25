97d14a7f387847107d1fe02ec39602af6ef904fb
Joe Torreggiani <joetorreggiani@gmail.com>
2022-07-24 17:28:55 -0700

    Make linter happy.

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,15 +1,9 @@
-import { useState, useEffect } from 'react';
-import moment from 'moment';
-
 // Elements
 import FeaturedEvents from 'components/elements/FeaturedEvents';
 import IntroductionSection from 'components/elements/IntroductionSection';
 import SearchEvents from 'components/elements/SearchEvents';
 import SearchForm from 'components/elements/SearchForm';
 import { useFeaturedEvents, useSearchEvents } from 'hooks/events';
-
-// Constants
-import { API_URL } from 'constants/urls';
 
 function HomePage() {
   const featuredEvents = useFeaturedEvents();
