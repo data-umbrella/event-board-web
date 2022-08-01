309d5395d62c772015915a1c85591a05539ee7cf
Carsdan Dvorachek <67628009+ChuckDvchek@users.noreply.github.com>
2022-08-01 16:56:12 -0400

    added the event id as a key for each map marker

--- src/components/elements/EventMapView.jsx
+++ src/components/elements/EventMapView.jsx
@@ -5,10 +5,10 @@ function EventMapView({ events }) {
 
   // Example list of events
   const mockEvents = [
-    {latitude: 0, longitude: 14},
-    {latitude: 45, longitude: 54},
-    {latitude: 40, longitude: 90},
-    {latitude: 28, longitude: 4},
+    {latitude: 0, longitude: 14, id: "event-1"},
+    {latitude: 45, longitude: 54, id: "event-2"},
+    {latitude: 40, longitude: 90, id: "event-3"},
+    {latitude: 28, longitude: 4, id: "event-4"},
   ]
 
   const [position, setPosition] = useState({center:[18,0], zoom:2.5});
@@ -19,7 +19,7 @@ function EventMapView({ events }) {
       <Map height={500} center={position.center} zoom={position.zoom} onBoundsChanged={({ newCenter, newZoom }) => setPosition({center: newCenter, zoom: newZoom})}>
         {mockEvents.map((marker) => {
           return (
-            <Marker width="30" anchor={[marker.latitude,marker.longitude]}/>
+            <Marker key={marker.id} width="30" anchor={[marker.latitude,marker.longitude]}/>
           )
         })
         }
