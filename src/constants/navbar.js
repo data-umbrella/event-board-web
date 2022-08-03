ad45e71fb6efe05f44dbd7330973c2234c74476d
Victoria <victoriakarenlo@gmail.com>
2022-08-03 15:58:54 -0700

    First commit update navbar

---
+++ src/constants/navbar.js
@@ -0,0 +1,12 @@
+export const NAVBAR_EVENT_OPTIONS = [
+    { label: 'Events', value: 'events' },
+    { label: 'Event Calendar', value: 'event-calendar' },
+    { label: 'Post Event', value: 'post-event' },
+    { label: 'Weekly Digest', value: 'weekly-digest' },,
+  ]
+  
+  export const NAVBAR_SUPPORT_OPTIONS = [
+    { label: 'Support Us', value: 'support-us' },
+    { label: 'Donate', value: 'donate' },
+    { label: 'Sponsors', value: 'sponsors' },
+  ]
