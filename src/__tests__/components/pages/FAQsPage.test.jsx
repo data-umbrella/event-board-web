7c87a9681f5f6ceafe2b26259b7f4069cb02e459
Victoria <victoriakarenlo@gmail.com>
2022-08-04 15:16:39 -0700

    added test case

---
+++ src/__tests__/components/pages/FAQsPage.test.jsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import FAQsPage from 'components/pages/FAQsPage';
+
+describe('FAQs Page', () => {
+  it('renders placeholder text', () => {
+    render(<FAQsPage />);
+
+    expect(screen.getByText(/Frequently Asked Questions/i)).toBeInTheDocument();
+    expect(screen.getByText(/I could not enter information into the form/i)).toBeInTheDocument();
+  });
+});
