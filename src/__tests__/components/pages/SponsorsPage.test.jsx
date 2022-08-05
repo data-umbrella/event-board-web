7ae55f403fd94a5fcb5a424e10925b924f16f9d0
Yoline <92892101+YolineB@users.noreply.github.com>
2022-08-05 14:04:12 -0700

    Implement Sponsors Page (#48)
    * Add style and Sponsors element
    
    * Adjust for unit test error
    
    * add sponsor names
    
    * Update sponsor page
    
    * Adjust Sponsors.jsx function
    
    * Add text test
    
    * Add footer link
    
    Co-authored-by: Crystal Shamsi <crystallynnvoss@gmail.com>
    Co-authored-by: Victoria <victoriakarenlo@gmail.com>

--- src/__tests__/components/pages/SponsorsPage.test.jsx
+++ src/__tests__/components/pages/SponsorsPage.test.jsx
@@ -5,7 +5,10 @@ describe('Sponsors Page', () => {
   it('renders placeholder text', () => {
     render(<SponsorsPage />);
 
-    expect(screen.getByText(/Sponsors/i)).toBeInTheDocument();
+    expect(screen.getByText(/Sponsors!/i)).toBeInTheDocument();
     expect(screen.getByText(/If you would like to sponsor/i)).toBeInTheDocument();
+    expect(screen.getByText(/CZI/i)).toBeInTheDocument();
   });
+
+
 });
--- src/components/pages/SponsorsPage.jsx
+++ src/components/pages/SponsorsPage.jsx
@@ -1,9 +1,17 @@
+import sponsorsStyleClasses from 'styles/sponsors';
+import Sponsors from 'components/elements/Sponsors'
+
 function SponsorsPage() {
   return (
     <div className="container mx-auto p-10">
-      <h1>Sponsors</h1>
+      <h1 className="text-2xl font-bold">Thank you to our Sponsors!</h1>
+      <p className="text-lg pl-0.5 pt-2 pb-2">
+        If you would like to sponsor this Event Board, please submit a form <a className={sponsorsStyleClasses.hyperlinks} href="/pages">here</a>.
+      </p>
 
-      <p>If you would like to sponsor this Event Board, please submit a form here.</p>
+      <div className={sponsorsStyleClasses.sponsorsGroup}>
+        <Sponsors />
+      </div>
     </div>
   )
 }
