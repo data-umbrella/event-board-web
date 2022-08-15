13cdf8c88096e5b760a5d8e39213c9c2aa3c44e5
Joe Torreggiani <joetorreggiani@gmail.com>
2022-08-14 17:41:26 -0700

    Add support for image url form seeds.

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,16 @@
+import defaultThumbNail from 'assets/thumbnail.png';
+
 export function isValidURL(string) {
   /* eslint-disable */
   const res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g);
   /* eslint-enable */
   return (res !== null);
+}
+
+export function imageUrlForEvent(eventData) {
+  if (eventData.imageUrl) return eventData.imageUrl;
+
+  return eventData.imageFile === '' || !eventData.imageFile
+    ? defaultThumbNail
+    : eventData.imageFile
 }
