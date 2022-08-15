0eef02bb869e04661203f525c9d29103014e418d
Victoria <victoriakarenlo@gmail.com>
2022-08-15 15:33:10 -0700

    Navbar styling

--- src/components/elements/NavigationBar.jsx
+++ src/components/elements/NavigationBar.jsx
@@ -12,50 +12,50 @@ function NavbarMenu() {
   return (
     <div className="">
       <div className="inline-flex">
-        <div>
-          <div className="dropdown relative">
-            <a
-              className={navStyleClasses.navSelectButton} inline-flex
-              type="button"
-              id="dropdownMenuButton2"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
+
+        <div className="dropdown relative">
+          <a
+            className={navStyleClasses.navSelectButton} inline-flex
+            type="button"
+            id="dropdownMenuButton2"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            Events
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fas"
+              data-icon="caret-down"
+              className="w-2 ml-2"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 320 512"
             >
-              Events
-              <svg
-                aria-hidden="true"
-                focusable="false"
-                data-prefix="fas"
-                data-icon="caret-down"
-                className="w-2 ml-2"
-                role="img"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 320 512"
-              >
-                <path
-                  fill="currentColor"
-                  d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
-                ></path>
-              </svg>
-            </a>
-            <ul
-              className={navStyleClasses.navSelectMenu}
-              aria-labelledby="dropdownMenuButton2"
-            >
-              {NAVBAR_EVENT_OPTIONS.map(({ value, label, route }) => {
-                return (
-                  <li key="hello">
-                    <a href={route} key={value} value={value} className={navStyleClasses.navDropdownItem}>
-                      {label}
-                    </a>
-                  </li>
-                )
-              })}
-            </ul>
-          </div>
+              <path
+                fill="currentColor"
+                d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+              ></path>
+            </svg>
+          </a>
+          <ul
+            className={navStyleClasses.navSelectMenu}
+            aria-labelledby="dropdownMenuButton2"
+          >
+            {NAVBAR_EVENT_OPTIONS.map(({ value, label, route }) => {
+              return (
+                <li key="hello">
+                  <a href={route} key={value} value={value} className={navStyleClasses.navDropdownItem}>
+                    {label}
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
         </div>
+
       </div>
-      <div className=" inline-flex">
+      <div className="inline-flex">
         <div>
           <div className="dropdown relative">
             <a
@@ -116,13 +116,13 @@ function NavigationBar() {
     <>
       
       <div className="border-b border-black container mx-auto lg:block">
-        <div className={navStyleClasses.navContainer}>
+        <div className={navStyleClasses.navLink}>
           <div className="col-span-2">
             <Link to="/"><img src={logoImg} alt="logo" /></Link>
           </div>
 
           <div className="col-span-1"></div>
-          <div className="col-span-9">
+          <div className="inline-flex col-span-9">
             <NavbarMenu />
             <div className={navStyleClasses.navLink}>
               <a
