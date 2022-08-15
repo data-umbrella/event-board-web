decb3f3c2b89051614eb344eaccb370b54a7d9b5
Joe Torreggiani <joetorreggiani@gmail.com>
2022-08-14 21:57:12 -0700

    Add test for navigation bar auth logic.

--- src/__tests__/elements/NavigationBar.test.jsx
+++ src/__tests__/elements/NavigationBar.test.jsx
@@ -2,22 +2,59 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavigationBar from 'components/elements/NavigationBar';
-import { AuthProvider } from 'hooks/authentication';
+import authHooks, { AuthProvider } from 'hooks/authentication';
+
+jest.mock('hooks/authentication', () => ({
+  ...jest.requireActual('hooks/authentication'),
+  useAuth: () => ({
+    currentUser: {
+      isAuthenticated: false,
+    }
+  })
+}));
 
 describe('Navigation bar', () => {
-  it('renders expected links', () => {
-    render(
-      <AuthProvider>
-        <Router>
-          <NavigationBar />
-        </Router>
-      </AuthProvider>
-    );
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  
+  describe('unauthenticated', () => {
+    it('renders expected links', () => {
+      authHooks.useAuth = () => {
+        return { currentUser: { isAuthenticated: false } }
+      }
 
-    expect(screen.getByText(/Event Calendar/i).href).toMatch('/events/calendar');
-    expect(screen.getByText(/Weekly Digest/i).href).toMatch('/events/weekly-digest');
-    expect(screen.getByText(/Data Umbrella/i).href).toMatch('www.dataumbrella.org');
-    expect(screen.getByText(/Sponsors/i).href).toMatch('/sponsors');
-    expect(screen.getByText(/Sign In/i).href).toMatch('/sign-in');
+      render(
+        <AuthProvider>
+          <Router>
+            <NavigationBar />
+          </Router>
+        </AuthProvider>
+      );
+
+      expect(screen.getByText(/Event Calendar/i).href).toMatch('/events/calendar');
+      expect(screen.getByText(/Weekly Digest/i).href).toMatch('/events/weekly-digest');
+      expect(screen.getByText(/Data Umbrella/i).href).toMatch('www.dataumbrella.org');
+      expect(screen.getByText(/Sponsors/i).href).toMatch('/sponsors');
+      expect(screen.getByText(/Sign In/i).href).toMatch('/sign-in');
+    });
+  });
+
+  describe('authenticated', () => {
+    it('renders sign out link when authenticated', () => {
+      authHooks.useAuth = () => {
+        return { currentUser: { isAuthenticated: true } }
+      }
+      
+      render(
+        <AuthProvider>
+          <Router>
+            <NavigationBar />
+          </Router>
+        </AuthProvider>
+      );
+
+      expect(screen.getByText(/Sign Out/i)).toBeInTheDocument();
+    });
   });
 });
