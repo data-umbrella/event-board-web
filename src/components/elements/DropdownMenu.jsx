abc905b4ca481056dbffc797c858a7e46b40682c
Joe Torreggiani <joetorreggiani@gmail.com>
2022-08-23 21:50:10 -0700

    Make linter happy.

--- src/components/elements/DropdownMenu.jsx
+++ src/components/elements/DropdownMenu.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { navStyleClasses } from 'styles/navbar';
+import DownArrowIcon from 'components/elements/DownArrowIcon';
 
 function DropdownMenu({ label, options }) {
   return (
@@ -14,22 +15,7 @@ function DropdownMenu({ label, options }) {
             aria-expanded="false"
           >
             { label }
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              data-icon="caret-down"
-              className="w-2 ml-2"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 320 512"
-            >
-              <path
-                fill="currentColor"
-                d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
-              >
-              </path>
-            </svg>
+            <DownArrowIcon />
           </a>
 
           <ul
@@ -39,7 +25,11 @@ function DropdownMenu({ label, options }) {
             {options.map(({ value, label, route }) => {
               return (
                 <li key={value}>
-                  <Link to={route} value={value} className={navStyleClasses.navDropdownItem}>
+                  <Link
+                    to={route}
+                    value={value}
+                    className={navStyleClasses.navDropdownItem}
+                  >
                     {label}
                   </Link>
                 </li>
