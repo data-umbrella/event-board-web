bb15ed3313f310cdaabcc38184314eb1227c3f8e
Joe Torreggiani <joetorreggiani@gmail.com>
2022-09-05 15:27:59 -0700

    Include year in event group key.

--- src/utils/events.js
+++ src/utils/events.js
@@ -2,7 +2,7 @@ import moment from 'moment';
 
 export function groupEventsByMonth(events) {
   const newObj = events.reduce(function (acc, evt) {
-    const month = moment(evt.startDate).format('MMMM');
+    const month = moment(evt.startDate).format('MMMM YYYY');
 
     if (!acc[month]) {
       acc[month] = [];
