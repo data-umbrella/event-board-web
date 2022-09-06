09b270aa277eca1b1399b6e49ddb320e042a50d6
reshamas <reshama.stat@gmail.com>
2022-09-06 15:32:30 -0400

    add links to logos (DU & SPEC)

--- src/components/elements/AboutUs.jsx
+++ src/components/elements/AboutUs.jsx
@@ -7,6 +7,7 @@ const aboutUsList = [
   { name:'Data Umbrella', 
     about_img: DUlogo,
     style: 'border-t border-l border-b border-r border-black rounded-l',
+    url: 'https://www.dataumbrella.org/',
     info: `Data Umbrella is a global non-profit community for underrepresented persons 
     in data science that organizes data science events. You can support Data Umbrella’s 
     work by making a donation to the Data Umbrella Open Collective.` },
@@ -14,9 +15,8 @@ const aboutUsList = [
   { name: 'Sustainable Progress and Equity Collective', 
     about_img: SPEC,
     style: 'border-t border-r border-b border-black rounded-r',
-    info: `Sustainable Progress & Equality Collective (SPEC) is an open learning organization 
-    that empowers individuals to be catalysts for positive change. You can support SPEC’s work 
-    by making a donation to the SPEC Open Collective.` }
+    url: 'https://www.specollective.org/',
+    info: `Sustainable Progress & Equality Collective (SPEC) is an open learning organization that empowers individuals to be catalysts for positive change. You can support SPEC’s work by making a donation to the SPEC Open Collective.` }
 ];
 
 function AboutUs() {
@@ -31,6 +31,7 @@ function AboutUs() {
             >
               <div>
                 <img src={aboutUsInfo.about_img} alt="logo" className={`mx-auto`}/>
+                <a href={aboutUsInfo.name}>{aboutUsInfo.url}</a>;
               </div>
               <div>
                 {aboutUsInfo.info}
