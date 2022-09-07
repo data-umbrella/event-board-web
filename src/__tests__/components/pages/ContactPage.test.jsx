a591144e7a592aa1ee17f5aa2e54a1c0307b5d8b
Joe Torreggiani <joetorreggiani@gmail.com>
2022-09-06 20:25:37 -0700

    Fix broken test.

--- src/__tests__/components/pages/ContactPage.test.jsx
+++ src/__tests__/components/pages/ContactPage.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import ContactUsPage from 'components/pages/ContactPage';
+import { MemoryRouter } from 'react-router-dom';
 
 describe('Contact Us Page', () => {
   it('renders text', () => {
-    render(<ContactUsPage />);
+    render(
+      <MemoryRouter>
+        <ContactUsPage />
+      </MemoryRouter>
+    );
 
     expect(screen.getByText(/Contact Us/i)).toBeInTheDocument();
     expect(screen.getByText(/For feature suggestions, bug reports, etc/i))
