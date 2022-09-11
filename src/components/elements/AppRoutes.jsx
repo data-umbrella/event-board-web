7f22e7919485ade8b4f2760e6203d6369f9d1e03
Joe Torreggiani <joetorreggiani@gmail.com>
2022-09-10 20:41:18 -0700

    Add registration link sent page.

--- src/components/elements/AppRoutes.jsx
+++ src/components/elements/AppRoutes.jsx
@@ -18,6 +18,7 @@ import SponsorsPage from 'components/pages/SponsorsPage';
 import FAQsPage from 'components/pages/FAQsPage';
 import AboutPage from 'components/pages/AboutPage';
 import MagicLinkSentPage from 'components/pages/MagicLinkSentPage';
+import RegistrationLinkSentPage from 'components/pages/RegistrationLinkSentPage';
 import VerifyMagicLinkPage from 'components/pages/VerifyMagicLinkPage';
 import ContactPage from 'components/pages/ContactPage';
 import PrivacyPage from 'components/pages/PrivacyPolicyPage';
@@ -72,7 +73,7 @@ function AppRoutes() {
       <Route path="/about" element={<AboutPage />} />
       <Route
         path="/registration/confirmation"
-        element={<MagicLinkSentPage />}
+        element={<RegistrationLinkSentPage />}
       />
       <Route
         path="/sign-in/sent"
---
+++ src/components/pages/RegistrationLinkSentPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useQuery } from 'hooks/query-params';
+
+function RegistrationLinkSent() {
+  const queryParams  = useQuery();
+  const email = queryParams.get('email');
+
+  return (
+    <div className="container mx-auto text-center">
+      <div className="pt-40 pb-40">
+        <h1 className="font-bold text-4xl">
+          We&apos;ve sent you a link to confirm your account!
+        </h1>
+
+        <p className="mt-10 text-2xl">
+          Please check your email <span className="font-bold">{ email }</span>
+        </p>
+
+        <p className="mt-10 text-xl">
+          Clicking the link with confirm your account, redirect to the website, and sign you in.
+          You can safely close this tab.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default RegistrationLinkSent;
