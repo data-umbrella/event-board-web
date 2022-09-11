f906709fde2c2e493c53941e89c8e865d4a39b69
reshamas <reshama.stat@gmail.com>
2022-09-11 12:05:47 -0400

    added URLs to orgs and collectives

--- src/components/elements/AboutUs.jsx
+++ src/components/elements/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import aboutStyleClasses from 'styles/about';
+import formStyleClasses from "styles/forms";
 import DUlogo from 'assets/data-umbr-full-transp-logo.png';
 import SPEC from 'assets/spec-logo.png';
 
@@ -8,15 +9,19 @@ const aboutUsList = [
     about_img: DUlogo,
     style: 'border-t border-l border-b border-r border-black rounded-l',
     url: 'https://www.dataumbrella.org/',
-    info: `Data Umbrella is a global non-profit community for underrepresented persons 
+    name_collective: 'Data Umbrella Open Collective',
+    url_collective: 'https://opencollective.com/data-umbrella',
+    info: ` is a global non-profit community for underrepresented persons 
     in data science that organizes data science events. You can support Data Umbrella’s 
-    work by making a donation to the Data Umbrella Open Collective.` },
+    work by making a donation to the ` },
 
-  { name: 'Sustainable Progress and Equity Collective', 
+  { name: 'Sustainable Progress and Equity Collective (SPEC)', 
     about_img: SPEC,
     style: 'border-t border-r border-b border-black rounded-r',
     url: 'https://www.specollective.org/',
-    info: `Sustainable Progress & Equality Collective (SPEC) is an open learning organization that empowers individuals to be catalysts for positive change. You can support SPEC’s work by making a donation to the SPEC Open Collective.` }
+    name_collective: 'SPEC Open Collective',
+    url_collective: 'https://opencollective.com/spec',
+    info: ` is an open learning organization that empowers individuals to be catalysts for positive change. You can support SPEC’s work by making a donation to the ` }
 ];
 
 function AboutUs() {
@@ -30,12 +35,17 @@ function AboutUs() {
               className={`${aboutStyleClasses.aboutUsOrg} ${aboutUsInfo.style}`}
             >
               <div>
-                <img src={aboutUsInfo.about_img} alt="logo" className={`mx-auto`}/>
-                <a href={aboutUsInfo.name}>{aboutUsInfo.url}</a>;
+                <a href={aboutUsInfo.url}>
+                  <img src={aboutUsInfo.about_img} alt="logo" className={`mx-auto`}/>
+                </a>
               </div>
               <div>
-                {aboutUsInfo.info}
-              </div> 
+                <a className={formStyleClasses.hyperlinks} href={aboutUsInfo.url}>{aboutUsInfo.name}</a>
+                {aboutUsInfo.info}                 
+                <a className={formStyleClasses.hyperlinks} 
+                  href={aboutUsInfo.url_collective}>{aboutUsInfo.name_collective}
+                </a>.
+              </div>
             </div>
           )
         })}
