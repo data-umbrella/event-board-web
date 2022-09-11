1ee325c998d42a2de9c6bbc121ca239b8c876ef5
Joe Torreggiani <joetorreggiani@gmail.com>
2022-09-10 22:14:26 -0700

    Add event list to event calendar page.

--- src/components/pages/EventCalendarPage.jsx
+++ src/components/pages/EventCalendarPage.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import SearchForm from 'components/elements/SearchForm';
 import { useSearchEvents } from 'hooks/events';
 import EventCalendarView from 'components/elements/EventCalendarView';
+import EventListView from 'components/elements/EventListView';
 
 function EventCalendarPage() {
   const [searchResultEvents, setSearchFilters] = useSearchEvents();
 
   return (
-    <div className="mt-4 mb-12">
+    <div className="mt-4 mb-12 md:px-6">
       <div>
+        <h1 className="text-xl font-bold mb-2 mt-12">Search Events</h1>
         <SearchForm handleFormSubmit={setSearchFilters} />
       </div>
 
-      <div className="px-6">
+      <div className="hidden md:block">
         <EventCalendarView events={searchResultEvents} />
+      </div>
+
+      <div>
+        <EventListView events={searchResultEvents} />
       </div>
     </div>
   )
