a9770317625ef5f914875141fc476c0c86a7ef87
Joe Torreggiani <joetorreggiani@gmail.com>
2022-09-10 20:18:57 -0700

    Temporarily hide weekly digest nav link. (#157)
    * Temporarily hide weekly digest nav link.
    
    * Add inline comments documenting plan to add back weekly digest navigation.

--- src/constants/navbar.js
+++ src/constants/navbar.js
@@ -1,7 +1,8 @@
 export const NAVBAR_EVENT_OPTIONS = [
   { label: 'Event Calendar', value: 'event-calendar', route: '/events/calendar' },
   { label: 'Post Event', value: 'post-event', route: '/events/new' },
-  { label: 'Weekly Digest', value: 'weekly-digest', route: '/events/weekly-digest' },
+  // TODO: Uncomment when weekly digest is implement.
+  // { label: 'Weekly Digest', value: 'weekly-digest', route: '/events/weekly-digest' },
 ]
   
 export const NAVBAR_SUPPORT_OPTIONS = [
