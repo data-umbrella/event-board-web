0dcf2a893e15de4fd22074305ee2eebc4be2298d
Saige Cross <saigecrossdev@gmail.com>
2022-09-12 18:58:58 -0500

    WIP: open/close fully functional. Now just fix styling.

--- src/components/elements/MobileNav.jsx
+++ src/components/elements/MobileNav.jsx
@@ -1,18 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 function MobileNav() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <>
       <div className='text-right'>
-        <p>X</p>
+        <button onClick={() => setMenuOpen((prev) => !prev)}>X</button>
       </div>
-      <div className='relative'>
+      <div className={menuOpen ? 'relative' : 'invisible'}>
         <div style={{backgroundColor: "#1E2445"}} className="z-20 fixed top-0 right-0 pl-8 h-screen text-right w-4/5 bg-sky-700">
           <div className='text-white text-2xl grid grid-rows-11 gap-3 place-content-evenly'>
-            <a className='pb-12 pt-10'>
+            <button onClick={() => setMenuOpen((prev) => !prev)} className='pb-12 pt-10 text-right'>
               Close X
-            </a>
+            </button>
             <Link to="/sign-in">Sign In</Link><br />
             <Link to="/events/calendar">Event Calendar</Link><br />
             <Link to="/events/new">Post Event</Link><br/>
