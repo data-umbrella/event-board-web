09ad9b3dece229ee51cfb27c1f30458238dbfdd2
Victoria <victoriakarenlo@gmail.com>
2022-09-12 13:12:14 -0700

    Add Terms Page outline and styles

---
+++ src/styles/terms.js
@@ -0,0 +1,21 @@
+const termsStyleClasses = {
+  outlineHeader:`
+    text-l
+    font-semibold
+    mr-10
+    leading-loose
+    `,
+  
+  bodyHeader:`
+    text-xl
+    font-bold
+    `,
+
+  body:`
+    mb-10
+    leading-7
+    `,
+  
+};
+  
+export default termsStyleClasses;
