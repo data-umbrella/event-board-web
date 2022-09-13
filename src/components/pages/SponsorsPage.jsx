7940b44c4f81aef5c52d4ba7462aef8f6e65d6a8
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-12 17:58:15 -0700

    Correct 'here' hyperlink to contact page

--- src/components/pages/SponsorsPage.jsx
+++ src/components/pages/SponsorsPage.jsx
@@ -7,9 +7,9 @@ function SponsorsPage() {
     <div className="container mx-auto pt-5 md:pt-14 pb-56">
       <h1 className="hidden md:block text-2xl font-bold">Thank you to our Sponsors!</h1>
       <h1 className="visible md:hidden text-2xl pb-5">Sponsors</h1>
-      <p className="text-base pt-2 pb-2">
+      <p className="text-base md:text-lg pt-2 pb-2">
         If you would like to sponsor this Event Board, please submit a form&nbsp;
-        <a className={sponsorsStyleClasses.hyperlinks} href="/pages">here</a>.
+        <a className={sponsorsStyleClasses.hyperlinks} href="/contact">here</a>.
       </p>
 
       <div className={sponsorsStyleClasses.sponsorsGroup}>
