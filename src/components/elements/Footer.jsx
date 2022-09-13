d98f1a7f812fb38d553c80857984e6f0f98e7e12
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-13 10:29:35 -0700

    Link Meet the Team

--- src/components/elements/Footer.jsx
+++ src/components/elements/Footer.jsx
@@ -3,16 +3,16 @@ import logoImg from 'assets/logo-color.svg';
 
 function Footer() {
   return (
-    <footer className="block bg-du-indigo-900 min-h-60 text-white px-40 pt-9 pb-7">
-      <div className="grid lg:grid-cols-4 text-left">
+    <footer className="block bg-du-indigo-900 min-h-10 md:min-h-60 text-white md:px-40 pt-9 pb-7">
+      <div className="hidden md:grid grid-cols-4 text-left">
         <div>
           <img src={logoImg} alt="logo" className="h-32 w-40" />
         </div>
 
         <div>
           <ul className="grid lg:grid-rows-3 lg:gap-4">
-            <li className="mb-2"><a href="/about">About us</a></li>
-            <li className="mb-2">Meet the team</li>
+            <li className="mb-2"><a href="/about">About Us</a></li>
+            <li className="mb-2"><a href="/about">Meet the Team</a></li>
             <li className="mb-2"><a href="/contact">Contact Us</a></li>
           </ul>
         </div>
