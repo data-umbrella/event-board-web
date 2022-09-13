dec85ad38363db987e4fdfae16ab2a892586ff48
Yoline <92892101+YolineB@users.noreply.github.com>
2022-09-13 12:02:47 -0700

    Correct header tags

--- src/components/pages/SponsorsPage.jsx
+++ src/components/pages/SponsorsPage.jsx
@@ -6,7 +6,7 @@ function SponsorsPage() {
   return (
     <div className="container mx-auto pt-5 md:pt-14 pb-56 dark:text-[#F7FAFC]">
       <h1 className="hidden md:block text-2xl font-bold pb-6">Thank you to our Sponsors!</h1>
-      <h1 className="visible md:hidden text-2xl pb-5">Sponsors</h1>
+      <h2 className="visible md:hidden text-2xl pb-5">Sponsors</h2>
       <p className="text-base md:text-lg pt-2 pb-2">
         If you would like to sponsor this Event Board, please submit a form&nbsp;
         <a className={sponsorsStyleClasses.hyperlinks} href="/contact">here</a>.
