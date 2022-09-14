c3f67012f0e9b4e4b8390ebc87f6152ae26dd4bf
Victoria <victoriakarenlo@gmail.com>
2022-09-14 13:58:04 -0700

    Add darkmode styles to links and date

--- src/components/elements/EventsForMonth.jsx
+++ src/components/elements/EventsForMonth.jsx
@@ -8,17 +8,17 @@ function EventsForMonth({ events }) {
 
   return sortedEvents.map(evt => {
     return (
-      <div key={`${evt.id}-${evt.eventName}`} className="bg-white mb-2 px-2 py-2 rounded border border-gray-300">
+      <div key={`${evt.id}-${evt.eventName}`} className="bg-white dark:bg-du-indigo-900 mb-2 px-2 py-2 rounded border border-gray-300">
         <div className="grid grid-cols-2">
           <span className="text-left">
             <Link
               to={`/events/${evt.id}/details`}
-              className="text-du-purple-500 underline underline-offset-4 font-medium decoration-2"
+              className="text-du-purple-500 dark:text-du-lightPurple underline underline-offset-4 font-medium decoration-2"
             >
               { evt.eventName }
             </Link>
           </span>
-          <span className="text-right">
+          <span className="text-right dark:text-du-gray">
             {evt.eventType}
             {moment(evt.startDate).format('MMMM D, YYYY')}
           </span>
