30eaff5d54dcf81da2888229cc83b38c1f1cc153
Jack Kinsey <jack.w.kinsey@gmail.com>
2022-09-15 17:26:35 -0400

    fix code after merge conflicts

--- src/components/elements/SearchEventCard.jsx
+++ src/components/elements/SearchEventCard.jsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { imageUrlForEvent  } from 'utils/urls';
-<<<<<<< HEAD
-import defaultThumbnail from 'assets/thumbnail.png';
-=======
 import moment from 'moment';
->>>>>>> main
+import defaultThumbnail from 'assets/thumbnail.png';
 
 const styleClasses = {
   searchEventCard: `
@@ -35,61 +32,36 @@ function formatDate(date) {
 }
 
 function SearchEventCard({ eventData }) {
-<<<<<<< HEAD
   const [imageUrl, setImageUrl] = useState(imageUrlForEvent(eventData));
+  const startDate = formatDate(eventData.startDate);
 
   const handleImageError = () => {
     setImageUrl(defaultThumbnail);
 	};
-=======
-  const imageSrc = imageUrlForEvent(eventData);
-  const startDate = formatDate(eventData.startDate);
->>>>>>> main
 
   return (
 		<Link to={`/events/${eventData.id}/details`} className={styleClasses.searchEventCard}>
 			<div className="card-img" style={{ backgroundImage: `url('${imageUrl}')` }}>
-        {/**
-         * This is a hidden image that loads the same background image for the card,
-         * but allows us to listen for an error event. Upon an error, we can set the
-         * image URL used to the default image.
-         */}
-				<img src={imageUrl} onError={handleImageError} className="w-full" alt="logo" style={{display: "none"}}/>
+				{/**
+				 * This is a hidden image that loads the same background image for the card,
+				 * but allows us to listen for an error event. Upon an error, we can set the
+				 * image URL used to the default image.
+				 */}
+				<img src={imageUrl} onError={handleImageError} className="w-full" alt="logo" style={{ display: 'none' }} />
 			</div>
 
-<<<<<<< HEAD
-			<div className="p-8 h-40">
+			<div className="p-8">
 				<div className={styleClasses.searchEventCardDetail}>
-					<time className="text-neutral-500" dateTime="2018-07-07">
-						{eventData.startDate}
-					</time>
-					<p className="text-right text-neutral-500 pr-2">Free</p>
+					<time className="text-du-purple-600 dark:text-du-lightPurple md:col-span-2">{startDate}</time>
+					<p className="text-right text-du-purple-600 dark:text-du-lightPurple pr-2 md:col-span-1">Free</p>
 				</div>
 
 				<div>
-					<p className="mt-6 font-bold text-xl">{eventData.eventName}</p>
+					<p className="mt-6 font-bold text-base md:text-xl dark:text-du-gray">{eventData.eventName}</p>
 				</div>
 			</div>
 		</Link>
 	);
-=======
-      <div className="p-8">
-        <div className={styleClasses.searchEventCardDetail}>
-          <time className="text-du-purple-600 dark:text-du-lightPurple md:col-span-2">
-            {startDate}
-          </time>
-          <p className="text-right text-du-purple-600 dark:text-du-lightPurple pr-2 md:col-span-1">
-            Free
-          </p>
-        </div>
-
-        <div>
-          <p className="mt-6 font-bold text-base md:text-xl dark:text-du-gray">{ eventData.eventName }</p>
-        </div>
-      </div>
-    </Link>
-  )
->>>>>>> main
 }
 
 export default SearchEventCard;
