d667972c2e96612550021cfcc583fd6da2101c6a
Victoria Lo <victoriakarenlo@gmail.com>
2022-10-27 17:02:51 -0700

    Update About page styles

--- src/components/elements/AboutUs.jsx
+++ src/components/elements/AboutUs.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import aboutStyleClasses from 'styles/about';
-import DULogo from 'assets/data-umbr-full-transp-logo.png';
-import SPECLogo from 'assets/speclogoname.png';
-import twitter from 'assets/twitter.svg';
-import linkedin from 'assets/linkedin.svg';
-import github from 'assets/github.svg';
-import website from 'assets/website.svg';
-import websiteWhite from 'assets/websiteWhite.svg';
-import twitterWhite from 'assets/twitterWhite.svg';
-import linkedInWhite from 'assets/linkedInWhite.svg';
-import githubWhite from 'assets/githubWhite.svg';
+import React, { useState } from "react";
+import aboutStyleClasses from "styles/about";
+import DULogo from "assets/data-umbr-full-transp-logo.png";
+import SPECLogo from "assets/speclogoname.png";
+import twitter from "assets/twitter.svg";
+import linkedin from "assets/linkedin.svg";
+import github from "assets/github.svg";
+import website from "assets/website.svg";
+import websiteWhite from "assets/websiteWhite.svg";
+import twitterWhite from "assets/twitterWhite.svg";
+import linkedInWhite from "assets/linkedInWhite.svg";
+import githubWhite from "assets/githubWhite.svg";
 
 const IMAGE_MAP = {
   twitter,
@@ -19,38 +19,69 @@ const IMAGE_MAP = {
   twitterWhite,
   linkedInWhite,
   githubWhite,
-  websiteWhite
-}
+  websiteWhite,
+};
 
 const DATA_UMBRELLA_SOCIAL_MEDIA_LINKS = [
-  { name: 'website', darkName: 'websiteWhite', link: 'https://www.dataumbrella.org/' },
-  { name: 'twitter', darkName: 'twitterWhite', link: 'https://twitter.com/DataUmbrella?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor' },
-  { name: 'linkedin', darkName: 'linkedInWhite', link: 'https://www.linkedin.com/company/dataumbrella/' },
-]
+  {
+    name: "website",
+    darkName: "websiteWhite",
+    link: "https://www.dataumbrella.org/",
+  },
+  {
+    name: "twitter",
+    darkName: "twitterWhite",
+    link: "https://twitter.com/DataUmbrella?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+  },
+  {
+    name: "linkedin",
+    darkName: "linkedInWhite",
+    link: "https://www.linkedin.com/company/dataumbrella/",
+  },
+];
 
 const SPEC_SOCIAL_MEDIA_LINKS = [
-  { name: 'website',  darkName: 'websiteWhite', link: 'https://www.specollective.org/' },
-  { name: 'twitter', darkName: 'twitterWhite', link: 'https://mobile.twitter.com/specollective' },
-  { name: 'linkedin', darkName: 'linkedInWhite', link: 'https://www.linkedin.com/company/specollective/' },
-]
+  {
+    name: "website",
+    darkName: "websiteWhite",
+    link: "https://www.specollective.org/",
+  },
+  {
+    name: "twitter",
+    darkName: "twitterWhite",
+    link: "https://mobile.twitter.com/specollective",
+  },
+  {
+    name: "linkedin",
+    darkName: "linkedInWhite",
+    link: "https://www.linkedin.com/company/specollective/",
+  },
+];
 
 function ContributorSocialMediaLinks({ socialMediaLinks }) {
   const [colorTheme, setTheme] = useState(localStorage.theme);
-  window.addEventListener("themeChanged", () => setTheme(localStorage.theme))
+  window.addEventListener("themeChanged", () => setTheme(localStorage.theme));
 
   return (
     <div className="mt-4">
-      { socialMediaLinks.map(mediaLink => {
+      {socialMediaLinks.map((mediaLink) => {
         return (
           <div className="inline-block p-2" key={mediaLink.name}>
-            <a href={mediaLink.link}>
-              <img className="mx-auto h-10 w-10" src={colorTheme === 'light' ? IMAGE_MAP[mediaLink.name] : IMAGE_MAP[mediaLink.darkName]} />
+            <a href={mediaLink.link} target="_blank" rel="noreferrer">
+              <img
+                className="mx-auto h-10 w-10"
+                src={
+                  colorTheme === "light"
+                    ? IMAGE_MAP[mediaLink.name]
+                    : IMAGE_MAP[mediaLink.darkName]
+                }
+              />
             </a>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 function AboutUs() {
@@ -58,60 +89,67 @@ function AboutUs() {
     <div key="aboutUsgrid" className={aboutStyleClasses.aboutUsCards}>
       <div className="grid grid-rows-1 md:m-0 p-2 m-6 border-2 border-black dark:border-du-lightAqua dark:text-white dark:bg-slate-700 rounded bg-white">
         <div className="py-4">
-          <img src={DULogo} alt="logo" className={`mx-auto`}/>
+          <img src={DULogo} alt="logo" className={`mx-auto`} />
         </div>
         <div className="row-span-2 px-4 m-1 object-contain text-left">
           <a
             href="https://www.dataumbrella.org/"
             target="_blank"
             rel="noreferrer"
-            className="text-teal-500 underline dark:text-blue-700"
+            className="text-du-darkBlue underline dark:text-blue-700"
           >
             Data Umbrella
           </a>
-          &nbsp;is a global non-profit community for underrepresented persons in data science
-          that organizes data science events. You can support Data Umbrella&apos;s work by
-          making a donation to the&nbsp;
+          &nbsp;is a global non-profit community for underrepresented persons in
+          data science that organizes data science events. You can support Data
+          Umbrella&apos;s work by making a donation to the&nbsp;
           <a
             href="https://opencollective.com/data-umbrella"
             target="_blank"
             rel="noreferrer"
-            className="text-teal-500 underline dark:text-blue-700"
+            className="text-du-darkBlue underline dark:text-blue-700"
           >
             Data Umbrella Open Collective
-          </a>.
+          </a>
+          .
         </div>
-        <ContributorSocialMediaLinks socialMediaLinks={DATA_UMBRELLA_SOCIAL_MEDIA_LINKS} />
+        <ContributorSocialMediaLinks
+          socialMediaLinks={DATA_UMBRELLA_SOCIAL_MEDIA_LINKS}
+        />
       </div>
 
       <div className="grid grid-rows-1 md:m-0 p-2 m-6 border-2 border-black dark:border-du-lightAqua dark:text-white dark:bg-slate-700 rounded bg-white">
         <div className="py-4">
-          <img src={SPECLogo} alt="logo" className={`mx-auto`}/>
+          <img src={SPECLogo} alt="logo" className={`mx-auto`} />
         </div>
         <div className="row-span-2 px-4 m-1 object-contain text-left">
           <a
             href="https://www.specollective.org/"
             target="_blank"
             rel="noreferrer"
-            className="text-teal-500 underline dark:text-blue-700"
+            className="text-du-darkBlue underline dark:text-blue-700"
           >
             Sustainable Progress and Equality Collective (SPEC)
           </a>
-          &nbsp;is an open learning organization that empowers individuals to be catalysts for positive change.
-          You can support SPEC&apos;s work making a donation to&nbsp;
+          &nbsp;is an open learning organization that empowers individuals to be
+          catalysts for positive change. You can support SPEC&apos;s work making
+          a donation to&nbsp;
           <a
             href="https://opencollective.com/spec"
             target="_blank"
             rel="noreferrer"
-            className="text-teal-500 underline dark:text-blue-700"
+            className="text-du-darkBlue underline dark:text-blue-700"
           >
             SPEC&apos;s Open Collective
-          </a>.
+          </a>
+          .
         </div>
-        <ContributorSocialMediaLinks socialMediaLinks={SPEC_SOCIAL_MEDIA_LINKS} />
+        <ContributorSocialMediaLinks
+          socialMediaLinks={SPEC_SOCIAL_MEDIA_LINKS}
+        />
       </div>
     </div>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;
--- src/components/elements/Contributor.jsx
+++ src/components/elements/Contributor.jsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import aboutStyleClasses from 'styles/about';
-import twitter from 'assets/twitter.svg';
-import linkedin from 'assets/linkedin.svg';
-import github from 'assets/github.svg';
-import website from 'assets/website.svg';
-import logo from 'assets/logo.svg';
-import Reshama from 'assets/reshama.png'
-import Joe from 'assets/joe.png'
-import Ilia from 'assets/ilia.png'
-import Yoline from 'assets/yoline.png'
-import Victoria from 'assets/victoria.png'
-import Crystal from 'assets/crystal.png'
-import websiteWhite from 'assets/websiteWhite.svg';
-import twitterWhite from 'assets/twitterWhite.svg';
-import linkedInWhite from 'assets/linkedInWhite.svg';
-import githubWhite from 'assets/githubWhite.svg';
+import React, { useState } from "react";
+import aboutStyleClasses from "styles/about";
+import twitter from "assets/twitter.svg";
+import linkedin from "assets/linkedin.svg";
+import github from "assets/github.svg";
+import website from "assets/website.svg";
+import logo from "assets/logo.svg";
+import Reshama from "assets/reshama.png";
+import Joe from "assets/joe.png";
+import Ilia from "assets/ilia.png";
+import Yoline from "assets/yoline.png";
+import Victoria from "assets/victoria.png";
+import Crystal from "assets/crystal.png";
+import websiteWhite from "assets/websiteWhite.svg";
+import twitterWhite from "assets/twitterWhite.svg";
+import linkedInWhite from "assets/linkedInWhite.svg";
+import githubWhite from "assets/githubWhite.svg";
 
 const IMAGE_MAP = {
   twitter,
@@ -24,121 +24,198 @@ const IMAGE_MAP = {
   twitterWhite,
   linkedInWhite,
   githubWhite,
-  websiteWhite
-}
+  websiteWhite,
+};
 
 // Move into constants
 const contributorList = [
-  { name: 'Reshama Shaikh',
-    title: 'Event Board Project',
-    organization: 'Data Umbrella',
+  {
+    name: "Reshama Shaikh",
+    title: "Event Board Project",
+    organization: "Data Umbrella",
     image: Reshama,
     socialMediaLinks: [
-      { name: 'twitter', darkName: 'twitterWhite', link: 'https://twitter.com/reshamas?lang=en' },
-      { name: 'github', darkName: 'githubWhite', link: 'https://github.com/reshamas' },
-      { name: 'linkedin', darkName: 'linkedInWhite', link: 'https://www.linkedin.com/in/reshamas/' },
-    ]
+      {
+        name: "twitter",
+        darkName: "twitterWhite",
+        link: "https://twitter.com/reshamas?lang=en",
+      },
+      {
+        name: "github",
+        darkName: "githubWhite",
+        link: "https://github.com/reshamas",
+      },
+      {
+        name: "linkedin",
+        darkName: "linkedInWhite",
+        link: "https://www.linkedin.com/in/reshamas/",
+      },
+    ],
   },
   {
-    name: 'Joe Torreggiani',
-    title: 'Project Executive',
-    organization: 'SPEC',
+    name: "Joe Torreggiani",
+    title: "Project Executive",
+    organization: "SPEC",
     image: Joe,
     socialMediaLinks: [
-      { name: 'twitter', darkName: 'twitterWhite', link: 'https://twitter.com/joetorreggiani?lang=en' },
-      { name: 'github', darkName: 'githubWhite', link: 'https://github.com/jtorreggiani' },
-      { name: 'linkedin', darkName: 'linkedInWhite', link: 'https://www.linkedin.com/in/joetorreggiani/' },
-    ]
+      {
+        name: "twitter",
+        darkName: "twitterWhite",
+        link: "https://twitter.com/joetorreggiani?lang=en",
+      },
+      {
+        name: "github",
+        darkName: "githubWhite",
+        link: "https://github.com/jtorreggiani",
+      },
+      {
+        name: "linkedin",
+        darkName: "linkedInWhite",
+        link: "https://www.linkedin.com/in/joetorreggiani/",
+      },
+    ],
   },
-  { name: 'Ilia de Leon',
-    title: 'Design Lead',
-    organization: 'SPEC',
+  {
+    name: "Ilia de Leon",
+    title: "Design Lead",
+    organization: "SPEC",
     image: Ilia,
     socialMediaLinks: [
-      { name: 'github', darkName: 'githubWhite', link: 'https://example.com' },
-      { name: 'linkedin', darkName: 'linkedInWhite', link:'https://www.linkedin.com/in/iliadeleon/' },
-    ]
+      {
+        name: "github",
+        darkName: "githubWhite",
+        link: "https://github.com/iliadeleon",
+      },
+      {
+        name: "linkedin",
+        darkName: "linkedInWhite",
+        link: "https://www.linkedin.com/in/iliadeleon/",
+      },
+    ],
   },
-  { name: 'Yoline Banerjee',
-    title: 'Software Engineer',
-    organization: 'SPEC',
+  {
+    name: "Yoline Banerjee",
+    title: "Software Engineer",
+    organization: "SPEC",
     image: Yoline,
     socialMediaLinks: [
-      { name: 'github', darkName: 'githubWhite', link: 'https://example.com' },
-      { name: 'linkedin', darkName: 'linkedInWhite', link: 'https://www.linkedin.com/in/yolineb/' },
-    ]
+      {
+        name: "github",
+        darkName: "githubWhite",
+        link: "https://github.com/YolineB",
+      },
+      {
+        name: "linkedin",
+        darkName: "linkedInWhite",
+        link: "https://www.linkedin.com/in/yolineb/",
+      },
+    ],
   },
-  { name: 'Victoria Lo',
-    title: 'Software Engineer',
-    organization: 'SPEC',
+  {
+    name: "Victoria Lo",
+    title: "Software Engineer",
+    organization: "SPEC",
     image: Victoria,
     socialMediaLinks: [
-      { name: 'github', darkName: 'githubWhite', link: 'https://github.com/victoriaklo' },
-      { name: 'linkedin', darkName: 'linkedInWhite', link: 'https://www.linkedin.com/in/victoria-lo/' },
-    ]
+      {
+        name: "twitter",
+        darkName: "twitterWhite",
+        link: "https://twitter.com/victorialo",
+      },
+      {
+        name: "github",
+        darkName: "githubWhite",
+        link: "https://github.com/victoriaklo",
+      },
+      {
+        name: "linkedin",
+        darkName: "linkedInWhite",
+        link: "https://www.linkedin.com/in/victoria-lo/",
+      },
+    ],
   },
-  { name: 'Crystal Shamsi',
-    title: 'Software Engineer',
-    organization: 'SPEC',
+  {
+    name: "Crystal Shamsi",
+    title: "Software Engineer",
+    organization: "SPEC",
     image: Crystal,
     socialMediaLinks: [
-      { name: 'github', darkName: 'githubWhite', link: 'https://github.com/crystallynnvoss' },
-      { name: 'linkedin', darkName: 'linkedInWhite', link: 'https://www.linkedin.com/in/crystalshamsi/' },
-    ]
+      {
+        name: "github",
+        darkName: "githubWhite",
+        link: "https://github.com/crystallynnvoss",
+      },
+      {
+        name: "linkedin",
+        darkName: "linkedInWhite",
+        link: "https://www.linkedin.com/in/crystalshamsi/",
+      },
+    ],
   },
-  // {name:'CZI', sponsor_img:CZIImg, title:'', org:'', contact:{twitter:'', github:'', linkedin: '', website: ''}},
-  // {name: 'Digital Ocean', sponsor_img: DigitalOceanImg},
 ];
 
 // TODO: Move subcomponents in separate element files.
 function ContributorSocialMediaLinks({ contributorInfo }) {
   const [colorTheme, setTheme] = useState(localStorage.theme);
-  window.addEventListener("themeChanged", () => setTheme(localStorage.theme))
+  window.addEventListener("themeChanged", () => setTheme(localStorage.theme));
 
   return (
     <div className="mt-4">
-      { contributorInfo.socialMediaLinks.map(mediaLink => {
+      {contributorInfo.socialMediaLinks.map((mediaLink) => {
         return (
           <div className="inline-block p-1" key={mediaLink.name}>
-            <a href={mediaLink.link}>
-              <img className="mx-auto" src={colorTheme === 'light' ? IMAGE_MAP[mediaLink.name] : IMAGE_MAP[mediaLink.darkName]} />
+            <a href={mediaLink.link} target="_blank" rel="noreferrer">
+              <img
+                className="mx-auto"
+                src={
+                  colorTheme === "light"
+                    ? IMAGE_MAP[mediaLink.name]
+                    : IMAGE_MAP[mediaLink.darkName]
+                }
+              />
             </a>
           </div>
-        )
+        );
       })}
     </div>
-  )
+  );
 }
 
 // TODO: Move into separate component.
 function ContributorPhoto({ imageSrc }) {
   const src = imageSrc ? imageSrc : logo;
-  
-  return <img className="pt-4 pb-3" src={src} alt="logo" />
+
+  return <img className="pt-4 pb-3" src={src} alt="logo" />;
 }
 
 function Contributor() {
   return (
     <div>
       <div className={aboutStyleClasses.contributorGrid}>
-        {contributorList.map(contributorInfo => {
+        {contributorList.map((contributorInfo) => {
           return (
-            <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1" key={contributorInfo.name}>
+            <div
+              className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1"
+              key={contributorInfo.name}
+            >
               <div>
                 <ContributorPhoto imageSrc={contributorInfo.image} />
               </div>
               <div className="place-self-center my-2 text-left md:text-center dark:text-white">
-                <h3>{ contributorInfo.name }</h3>
-                <small>{ contributorInfo.title }</small><br />
-                <small>{ contributorInfo.organization }</small>
-                <ContributorSocialMediaLinks contributorInfo={contributorInfo} />
+                <h3>{contributorInfo.name}</h3>
+                <small>{contributorInfo.title}</small>
+                <br />
+                <small>{contributorInfo.organization}</small>
+                <ContributorSocialMediaLinks
+                  contributorInfo={contributorInfo}
+                />
               </div>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
 
-export default Contributor
+export default Contributor;
