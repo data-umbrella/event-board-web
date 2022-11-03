db4c2756a323b8711fcb44c95daf903348738045
Crystal Shamsi <crystallynnvoss@gmail.com>
2022-11-03 11:39:57 -0600

    Add hover to event
    Update popup styling

--- src/__tests__/components/pages/EventCalendarPage.test.jsx
+++ src/__tests__/components/pages/EventCalendarPage.test.jsx
@@ -33,7 +33,8 @@ describe('Event Calendar Page', () => {
     );
 
     expect(screen.getByText(/Advanced Filters/i)).toBeInTheDocument();
-    expect(screen.getAllByText(/Example event name/i)).toHaveLength(2);
+    expect(screen.getAllByText(/Example event name/i)).toHaveLength(3);
+    //updated expectation toHaveLength as 3 to accommodate for popup info
   });
 
   describe('Search', () => {
