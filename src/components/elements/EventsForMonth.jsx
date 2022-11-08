274590412361dfa937381bb40b5cb46f3fe6052d
Saige Cross <saigecrossdev@gmail.com>
2022-11-08 11:00:43 -0600

    Fixed small issues, next is date range and formatting list view.

--- src/components/elements/EventsForMonth.jsx
+++ src/components/elements/EventsForMonth.jsx
@@ -23,28 +23,28 @@ function formatEventTags(tags) {
 
 function EventsForMonth({ events }) {
   const sortedEvents = events.sort(sortByDate);
-
+  
   return sortedEvents.map(evt => {
     const eventTags = formatEventTags(evt.tags);
-
+    
     return (
       <div key={`${evt.id}-${evt.eventName}`} className="border border-gray-300 bg-white dark:bg-du-indigo-900 dark:border-du-lightAqua mb-2 px-2 py-2 rounded">
-        <div className="grid grid-cols-2">
+        <div className="grid grid-cols-3 sm:grid-cols-2">
           <span className="text-left">
             <Link
               to={`/events/${evt.id}/details`}
               className="text-du-purple-500 dark:text-du-lightPurple underline underline-offset-4 font-medium decoration-2"
             >
-              { evt.eventName }
+              {moment(evt.startDate).format('MMMM D, YYYY')}
             </Link>
           </span>
-          <span className="text-right text-du-charcoal-gray dark:text-du-gray">
+          <span className="text-center sm:text-right text-du-charcoal-gray dark:text-du-gray">
             {eventTags.map(tag => (
               <span key={tag} className={styleClasses.tags}>{tag}</span>
             ))}
-            <span className="pl-2">
-              {moment(evt.startDate).format('MMMM D, YYYY')}
-            </span>
+          </span>
+          <span className="pl-2">
+            { evt.eventName }
           </span>
         </div>
       </div>
