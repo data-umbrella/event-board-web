1e7c93786f98b031d9311d9d8cd25f014a0c7b85
Saige Cross <saigecrossdev@gmail.com>
2022-11-09 09:34:49 -0600

    Reversed order for list results on phone to tablet.

--- src/components/elements/EventsForMonth.jsx
+++ src/components/elements/EventsForMonth.jsx
@@ -22,14 +22,14 @@ function formatEventTags(tags) {
   return tags.trim().split(',').filter(tag => tag !== '');
 }
 
-function DateRange(start, end) {
-  const dateStart = start.start.split(' ')[0]
-  const dateEnd = start.end.split(' ')[0]
+function DateRange(event) {
+  const dateStart = event.start.split(' ')[0]
+  const dateEnd = event.end.split(' ')[0]
 
   if(dateStart === dateEnd) {
-    return <p>{start.start.slice(0, -6) + ' - ' + start.end.substring(start.end.indexOf(' ') + 1)}</p>
+    return <p>{event.start.slice(0, -6) + ' - ' + event.end.substring(event.end.indexOf(' ') + 1)}</p>
   }
-  return <p>{start.start + ' - ' + start.end}</p>
+  return <p>{event.start + ' - ' + event.end}</p>
 }
 
 
@@ -45,27 +45,27 @@ function EventsForMonth({ events }) {
 
     return (
       <div key={`${evt.id}-${evt.eventName}`} className="border border-gray-300 bg-white dark:bg-du-indigo-900 dark:border-du-lightAqua mb-2 px-2 py-2 rounded">
-        <div className="grid grid-cols-3 sm:grid-cols-2">
-          <span className="text-left">
-            <Link
-              to={`/events/${evt.id}/details`}
-              className="text-du-purple-500 dark:text-du-lightPurple underline underline-offset-4 font-medium decoration-2"
-            >
-              <DateRange 
-                start={dateStart}
-                end={dateEnd}
-              />
-
-            </Link>
+        <div className="grid grid-cols-3 sm:flex sm:flex-row-reverse sm:space-bet sm:place-content-between">
+          <span className="text-left truncate text-sm sm:text-base sm:col-start-2">
+            <DateRange 
+              start={dateStart}
+              end={dateEnd}
+            />
           </span>
           <span className="text-center sm:text-right text-du-charcoal-gray dark:text-du-gray">
             {eventTags.map(tag => (
               <span key={tag} className={styleClasses.tags}>{tag}</span>
             ))}
           </span>
-          <span className="pl-2">
-            { evt.eventName }
-          </span>
+          <Link
+            to={`/events/${evt.id}/details`}
+            className="text-du-purple-500 dark:text-du-lightPurple underline underline-offset-4 font-medium decoration-2 truncate"
+          >
+            <span className="pl-2">
+              { evt.eventName }
+            </span>
+          </Link>
+
         </div>
       </div>
     )
