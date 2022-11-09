3827c0737aa85848367981ca2b46c80ae568d9fb
Saige Cross <saigecrossdev@gmail.com>
2022-11-09 12:22:15 -0600

    Mapping event colors appropriately

--- src/components/elements/EventsForMonth.jsx
+++ src/components/elements/EventsForMonth.jsx
@@ -16,6 +16,15 @@ const styleClasses = {
   `
 }
 
+function RenderEventType(data) {
+  // debugger
+  // eslint-disable-next-line no-console
+  console.log(data.data) 
+  if(data.data === 'conference') {
+    return <span className={`inline-block mr-1 pl-2 pr-2 rounded bg-event-tags-${data.data} text-white`}>{data.data}</span>
+  } 
+}
+
 function formatEventTags(tags) {
   if (tags === null || tags === undefined) return [];
 
@@ -45,18 +54,7 @@ function EventsForMonth({ events }) {
 
     return (
       <div key={`${evt.id}-${evt.eventName}`} className="border border-gray-300 bg-white dark:bg-du-indigo-900 dark:border-du-lightAqua mb-2 px-2 py-2 rounded">
-        <div className="grid grid-cols-3 sm:flex sm:flex-row-reverse sm:space-bet sm:place-content-between">
-          <span className="text-left truncate text-sm sm:text-base sm:col-start-2">
-            <DateRange 
-              start={dateStart}
-              end={dateEnd}
-            />
-          </span>
-          <span className="text-center sm:text-right text-du-charcoal-gray dark:text-du-gray">
-            {eventTags.map(tag => (
-              <span key={tag} className={styleClasses.tags}>{tag}</span>
-            ))}
-          </span>
+        <div className="flex place-content-between">
           <Link
             to={`/events/${evt.id}/details`}
             className="text-du-purple-500 dark:text-du-lightPurple underline underline-offset-4 font-medium decoration-2 truncate"
@@ -65,7 +63,24 @@ function EventsForMonth({ events }) {
               { evt.eventName }
             </span>
           </Link>
-
+          <span className="text-center text-du-charcoal-gray dark:text-du-gray">
+            <span className="hidden sm:inline-block">
+              {eventTags.map(tag => (
+                <span key={tag} className={styleClasses.tags}>{tag}</span>
+              ))}
+            </span>
+            <span>
+              <RenderEventType 
+                data={evt.eventType}
+              />
+            </span>
+          </span>
+          <span className="text-left">
+            <DateRange 
+              start={dateStart}
+              end={dateEnd}
+            />
+          </span>
         </div>
       </div>
     )
