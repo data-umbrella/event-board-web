612c3ad993de99f6b2b1f5fcb037073234dc84df
Saige Cross <saigecrossdev@gmail.com>
2022-11-10 09:44:08 -0600

    reverted dateRange into EventsForMonth and corrected the util function.

--- src/components/elements/EventsForMonth.jsx
+++ src/components/elements/EventsForMonth.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 import { sortByDate } from 'utils/dates';
-import { DateRange } from 'utils/dateRange'
+// import { DateRange } from 'utils/dateRange'
 
 
 const styleClasses = {
@@ -15,6 +15,16 @@ const styleClasses = {
     rounded
     text-white
   `
+}
+
+function DateRange(event) {
+  const dateStart = event.start.split(' ')[0]
+  const dateEnd = event.end.split(' ')[0]
+
+  if(dateStart === dateEnd) {
+    return <p className="truncate">{event.start.slice(0, -6) + ' - ' + event.end.substring(event.end.indexOf(' ') + 1)}</p>
+  }
+  return <p className="truncate">{event.start + ' - ' + event.end}</p>
 }
 
 function RenderEventType(data) {
--- src/utils/dateRange.js
+++ src/utils/dateRange.js
@@ -1,12 +1,3 @@
-
 export function DateRange(event) {
-  const dateStart = event.start.split(' ')[0]
-  const dateEnd = event.end.split(' ')[0]
-
-  if(dateStart === dateEnd) {
-    // eslint-disable-next-line react/react-in-jsx-scope
-    return <p className="truncate">{event.start.slice(0, -6) + ' - ' + event.end.substring(event.end.indexOf(' ') + 1)}</p>
-  }
-  // eslint-disable-next-line react/react-in-jsx-scope
-  return <p className="truncate">{event.start + ' - ' + event.end}</p>
+  event.start.slice(0, -6) + ' - ' + event.end.substring(event.end.indexOf(' ') + 1)
 }
