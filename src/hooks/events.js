09c615df4bed80309057119081b64d1e85e89890
Victoria Lo <52011611+victoriaklo@users.noreply.github.com>
2022-11-14 11:30:41 -0800

    Issue 258: Add pagination and styling in the front end (#263)
    * Update packages and changes to event hooks
    
    * Handle page change and filter hooks
    
    * refactor hooks for pagination
    
    * Pagination styling
    
    * Add styles for pagination
    
    * Fix CI failure
    
    * Update events per page

--- src/hooks/events.js
+++ src/hooks/events.js
@@ -1,43 +1,42 @@
-import { EVENTS_URL } from 'constants/urls';
-import camelcaseKeys from 'camelcase-keys';
-import moment from 'moment';
-import { useEffect, useState } from 'react';
-import { arrayifyTags } from 'utils/strings';
+import { EVENTS_URL } from "constants/urls";
+import camelcaseKeys from "camelcase-keys";
+import moment from "moment";
+import { useEffect, useState } from "react";
+import { arrayifyTags } from "utils/strings";
 import {
   fetchEventsForSearchFilters,
   fetchFeaturedEvents,
-} from 'services/events';
-import { DEFAULT_DATE_FORMAT } from 'constants/dates';
+} from "services/events";
+import { DEFAULT_DATE_FORMAT } from "constants/dates";
 
 export function useEvent(eventId) {
   const [evt, setEvent] = useState();
 
   useEffect(() => {
-    async function fetchEvent() {
-      let json;
-
-      if (sessionStorage.getItem(eventId)) {
-        json = JSON.parse(sessionStorage.getItem(eventId));
-      } else {
-        try {
-          const requestURL = `${EVENTS_URL}/${eventId}/`;
-          const response = await fetch(requestURL);
-          json = await response.json();
-        } catch (e) {
-          // TODO: Add graceful error states
-          // console.error(e.message);
-        }
-      }
-
-      const result = camelcaseKeys(json);
-      result.tags = arrayifyTags(result.tags);
-      result.speakers = arrayifyTags(result.speakers);
-      result.accessibilityOptions = arrayifyTags(result.accessibilityOptions);
-
-      setEvent(result);
-    }
     fetchEvent();
   }, [eventId]);
+
+  async function fetchEvent() {
+    let json;
+
+    if (sessionStorage.getItem(eventId)) {
+      json = JSON.parse(sessionStorage.getItem(eventId));
+    } else {
+      try {
+        const requestURL = `${EVENTS_URL}/${eventId}/`;
+        const response = await fetch(requestURL);
+        json = await response.json();
+      } catch (e) {
+        throw new Error('Fetching the events failed.')
+      }
+    }
+    const result = camelcaseKeys(json);
+    result.tags = arrayifyTags(result.tags);
+    result.speakers = arrayifyTags(result.speakers);
+    result.accessibilityOptions = arrayifyTags(result.accessibilityOptions);
+
+    setEvent(result);
+  }
 
   return evt;
 }
@@ -46,21 +45,24 @@ export function useFeaturedEvents() {
   const [featuredEvents, setFeaturedEvents] = useState([]);
 
   useEffect(() => {
-    async function triggerRequest() {
-      const events = await fetchFeaturedEvents();
-      setFeaturedEvents([...events]);
-    }
     triggerRequest();
   }, []);
+
+  async function triggerRequest() {
+    const events = await fetchFeaturedEvents();
+    setFeaturedEvents([...events]);
+  }
 
   return featuredEvents;
 }
 
-export function useSearchEvents() {
+export function useSearchEvents({ pageSize, page }) {
   const [searchFilters, setSearchFilters] = useState({
     startDate: moment().format(DEFAULT_DATE_FORMAT),
-    endDate: moment().add(5, 'months').format(DEFAULT_DATE_FORMAT),
-    search: '',
+    endDate: moment().add(5, "months").format(DEFAULT_DATE_FORMAT),
+    search: "",
+    pageSize,
+    page,
   });
   const [searchResultEvents, setSearchResultEvents] = useState([]);
 
@@ -72,21 +74,27 @@ export function useSearchEvents() {
     });
   }
 
-  function clearFilters () {
+  function clearFilters() {
     setSearchFilters({
       startDate: moment().format(DEFAULT_DATE_FORMAT),
-      endDate: moment().add(5, 'months').format(DEFAULT_DATE_FORMAT),
-      search: '',
+      endDate: moment().add(5, "months").format(DEFAULT_DATE_FORMAT),
+      search: "",
     });
   }
 
   useEffect(() => {
-    async function fetchSearchEvents() {
-      const result = await fetchEventsForSearchFilters(searchFilters);
-      setSearchResultEvents(result);
-    }
     fetchSearchEvents();
   }, [searchFilters]);
 
-  return [searchResultEvents, handleSearchFiltersChange, clearFilters];
+  async function fetchSearchEvents() {
+    const result = await fetchEventsForSearchFilters(searchFilters);
+    setSearchResultEvents(result);
+  }
+
+  return [
+    searchResultEvents,
+    handleSearchFiltersChange,
+    clearFilters,
+    searchFilters,
+  ];
 }
