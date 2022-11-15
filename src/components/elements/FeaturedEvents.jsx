6f550607e7c67bec5abce9f3964cbc2cc728bdf6
Yoline <92892101+YolineB@users.noreply.github.com>
2022-11-15 13:04:41 -0800

    Fix screen size display

--- src/components/elements/FeaturedEvents.jsx
+++ src/components/elements/FeaturedEvents.jsx
@@ -6,17 +6,17 @@ import 'react-multi-carousel/lib/styles.css';
 
 const responsive = {
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 3000, min: 1025 },
     items: 3,
     slidesToSlide: 3 // optional, default to 1.
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
+    breakpoint: { max: 1024, min: 641 },
     items: 2,
     slidesToSlide: 2 // optional, default to 1.
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 },
+    breakpoint: { max: 640, min: 0 },
     items: 1,
     slidesToSlide: 1 // optional, default to 1.
   }
