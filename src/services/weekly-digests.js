14edfcabf747ae50bd4fba1170d4b643ea2cd851
Saige Cross <saigecrossdev@gmail.com>
2022-11-15 15:31:34 -0600

    First commit - added DELETE method

--- src/services/weekly-digests.js
+++ src/services/weekly-digests.js
@@ -13,3 +13,17 @@ export async function postWeeklyDigestEmail(data) {
     return response.json();
   }
 }
+
+export async function deletetWeeklyDigestEmail(data) {
+  const response = await fetch(WEEKLY_DIGESTS_URL, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(data),
+  });
+
+  if (response.status === 201) {
+    return true;
+  } else {
+    return response.json();
+  }
+}
