5842a7eeb9146dc39bdd702a8e33208807944ce6
Joe Torreggiani <joetorreggiani@gmail.com>
2022-11-21 22:47:29 -0500

    Handle ios cookies issue.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,17 @@
 import camelcaseKeys from 'camelcase-keys';
+import { iOSPlatform } from 'utils/devices';
+import { DEFAULT_HEADERS } from 'constants/api';
+import { getUserToken } from 'utils/sessions';
+
+export function buildAuthenticatedHeaders() {
+  const headers = DEFAULT_HEADERS;
+
+  if (iOSPlatform()) {
+    headers['Authorization'] = `Token ${getUserToken()}`;
+  }
+
+  return headers;
+}
 
 export function parseAPIJSON(jsonPayload) {
   let results;
