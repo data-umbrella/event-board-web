ad95db8628a47cb671c9a4133f38fa9baf99bc4c
Yoline <92892101+YolineB@users.noreply.github.com>
2023-02-13 23:51:03 -0800

    Add price default to eventProperties

--- src/constants/events.js
+++ src/constants/events.js
@@ -37,6 +37,7 @@ export const eventProperties = {
   'eventNotes': '',
   'organizationName': '',
   'organizationUrl': '',
+  'price': '',
   'published': false,
   'region': '',
   'registrationEndDate': '',
