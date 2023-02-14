6053f024dec002fc4916401ed94d61ba19523c91
Yoline <92892101+YolineB@users.noreply.github.com>
2023-02-14 09:12:40 -0800

    Adjust format

--- src/__tests__/utils/price.test.js
+++ src/__tests__/utils/price.test.js
@@ -2,30 +2,21 @@ import { formatPrice  } from 'utils/strings';
 
 describe('formatPriceFromEvent', () => {
   it('returns a correctly formatted price for free', () => {
-    const event = 
-      {
-        price: 'free',
-      }
-
+    const event = { price: 'free' }
+    
     const result = formatPrice(event.price)
     expect(result).toMatch('Free')
   })
 
   it('returns a correctly formatted price for paid', () => {
-    const event = 
-      {
-        price: 'paid',
-      }
+    const event = { price: 'paid' }
 
     const result = formatPrice(event.price)
     expect(result).toMatch('Paid')
   })
 
   it('returns a correctly formatted price for field left null', () => {
-    const event = 
-      {
-        price: null,
-      }
+    const event = { price: null }
 
     const result = formatPrice(event.price)
     expect(result).toMatch('')
