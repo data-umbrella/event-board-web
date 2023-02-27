f78da9d2a6688daad8d01e4a591c5ca9d86dfe45
Yoline <92892101+YolineB@users.noreply.github.com>
2023-02-27 14:01:12 -0800

    Correct styling

--- src/components/elements/AddEventToCalendar.jsx
+++ src/components/elements/AddEventToCalendar.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-// import 'add-to-calendar-button';
 import { atcb_action } from "add-to-calendar-button";
 import calendarIcon from '../../assets/ui/calendarIcon.png';
-// import customCss from '../../styles/calendar.css'
 
 const calendars = {
   'Google': 'Google',
@@ -11,81 +9,50 @@ const calendars = {
   'Outlook.com': 'Outlook'
 }
 
-function AddEventToCalendar() {
-  const calendarKeys = Object.keys(calendars)
-
-  const config = {
-    name: "[Reminder] Test the Add to Calendar Button",
-    description: "Check out the maybe easiest way to include Add to Calendar Buttons to your web projects:[br]→ [url]https://add-to-calendar-button.com/",
-    startDate: "2023-03-02",
-    startTime: "10:15",
-    endTime: "23:30",
-    options: ["Google|My custom label", "iCal"],
-    timeZone: "America/Los_Angeles"
-  };
+function CalendarBtn({ calendar, evt }) {
 
   function handleClick() {
-    atcb_action(config, document.getElementById('my-default-button'));
+    atcb_action(config, document.getElementById({ calendar }));
   }
+  const config = {
+    name: evt.eventName,
+    description: evt.description,
+    startDate: evt.startDate,
+    endDate: evt.endDate,
+    startTime: evt.startTime,
+    endTime: evt.endTime,
+    location: evt.location,
+    timeZone: evt.timeZone,
+    options: [calendar],
+    buttonStyle:"flat",
+  };
+
+  return(
+    <button
+      id={`${ calendar }`}
+      className="px-3 text-xl font-normal underline dark:text-white"
+      onClick={handleClick}
+    >
+      {calendars[calendar]}
+    </button>
+  )
+}
+
+function AddEventToCalendar({ evt }) {
+  const calendarKeys = Object.keys(calendars)
 
   return (
-    <section className="w-2/5 flex mb-10">
-      <img src={calendarIcon} alt="calendarIcon" className="w-10 h-10"/>
-      
-      {calendarKeys.map( key => {
-
+    <section className="w-2/5 flex mb-10 gap-2 content-center items-center">
+      <img src={calendarIcon} alt="calendarIcon" className="w-10 h-10 dark:text-whitet"/>   
+      {calendarKeys.map( (calendar, i) => {
         return(
-          <button
-            id={`${key}`}
-            key={`${key}`}
-            style={{
-              background: '#ffa255',
-              color: '#000',
-              padding: '8px 16px',
-              height: 'fit-content',
-              display: 'block',
-              borderRadius: '21px'
-            }}
-            onClick={handleClick}
-          >
-            {calendars[key]}
-          </button>
+          <div key={i}>
+            <CalendarBtn calendar={calendar} evt={evt}/>
+          </div>
         )
       })}
     </section>
   );
 }
-
-// function AddEventToCalendar({ evt }) {
-//   const calendarKeys = Object.keys(calendars)
-//   return (
-//     <section className="w-2/5 flex mb-10">
-//       <img src={calendarIcon} alt="calendarIcon" className="w-10 h-10"/>
-//       {calendarKeys.map(key => {
-//         return (
-//           <div key={key}>
-//             <add-to-calendar-button
-//               name={evt.eventName}
-//               options={`${key}`}
-//               location={evt.location}
-//               startDate={evt.startDate}
-//               endDate={evt.endDate}
-//               startTime={evt.startTime}
-//               endTime={evt.endTime}
-//               timeZone={evt.timezone}
-//               // customCss={customCss}
-//               inline
-//               hideBackground
-//               hideIconButton
-//               buttonsList
-//               buttonStyle="flat"
-//               size="2"
-//               label={calendars[key]}
-//             ></add-to-calendar-button>
-//           </div>
-//         )})}
-//     </section>
-//   )
-// }
 
 export default AddEventToCalendar;
