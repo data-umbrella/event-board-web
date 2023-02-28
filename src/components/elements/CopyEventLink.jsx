b8482bcc8e952a0c3ac9a8c78a1d852933f84295
Yoline <92892101+YolineB@users.noreply.github.com>
2023-02-28 14:08:22 -0800

    Add confirmation check for link copy

--- src/components/elements/CopyEventLink.jsx
+++ src/components/elements/CopyEventLink.jsx
@@ -1,25 +1,31 @@
 import React, { useState } from 'react';
 import copyLinkIcon from 'assets/ui/copyLinkIcon.svg'
 import copyLinkIconDark from 'assets/ui/copyLinkIconDark.svg'
+import { FcCheckmark }from 'react-icons/fc';
 
 function CopyEventLink() {
   const url = location.href
   const [colorTheme, setTheme] = useState(localStorage.theme);
+  const [copyConfirmed, setCopyConfirmed] = useState(false);
+
   window.addEventListener("themeChanged", () => setTheme(localStorage.theme));
  
   function handleCopyClick(){
     navigator.clipboard.writeText(url).then(
       () => {
-        alert('copied!')
+        setCopyConfirmed(true);
+        setTimeout(() => {
+          setCopyConfirmed(false);
+        }, 2000);
       },
       (e) => {
-        if(e) {alert(e)}
+        if(e) {alert('Copy error')}
       }
     );
   }
 
   return (
-    <section className="flex flex-row gap-x-7 items-center text-xl dark:text-white font-medium">
+    <section className="relative flex flex-row gap-x-7 items-center text-xl dark:text-white font-medium">
       Copy Link
       <button onClick={()=> {handleCopyClick()}}>
         <img 
@@ -27,6 +33,11 @@ function CopyEventLink() {
           alt="copyLinkIcon" 
           className="h-11"/> 
       </button>
+      { copyConfirmed && 
+        <div className="absolute inset-y-0 right-0"> 
+          { <FcCheckmark/> } 
+        </div> 
+      }
     </section>
   );
 }
