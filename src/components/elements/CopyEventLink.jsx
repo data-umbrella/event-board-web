5c7fd92623a28120f8ed6368d1fbb09cbeb39f8e
Yoline <92892101+YolineB@users.noreply.github.com>
2023-02-28 12:22:13 -0800

    Add copy functionality

--- src/components/elements/CopyEventLink.jsx
+++ src/components/elements/CopyEventLink.jsx
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import copyLinkIcon from 'assets/ui/copyLinkIcon.svg'
 import copyLinkIconDark from 'assets/ui/copyLinkIconDark.svg'
 
 function CopyEventLink() {
   const [colorTheme, setTheme] = useState(localStorage.theme);
   window.addEventListener("themeChanged", () => setTheme(localStorage.theme));
+  const url = useLocation().pathname;
+
+ function handleCopyClick(){
+    navigator.clipboard.writeText(url).then(
+      () => {
+      console.log('url copied', url)
+      },
+      (e) => {
+      if(e) {console.log(e)}
+      }
+    );
+    return (
+      alert('copied!')
+    );  
+}
+
+
+  // const handleCopyClick =  useEffect(() => {
+  //   const url = useLocation().pathname;
+  
+  //   navigator.clipboard.writeText(url);
+  
+  //   return (
+  //     alert('copied!')
+  //   );
+  
+  // }, []);
 
   return (
     <section className="flex flex-row gap-x-7 items-center text-xl dark:text-white font-medium">
       Copy Link
-      <img 
-        src={colorTheme === 'light' ? copyLinkIcon : copyLinkIconDark} 
-        alt="copyLinkIcon" 
-        className="h-11"/> 
+      <button onClick={()=> {handleCopyClick()}}>
+        <img 
+          src={colorTheme === 'light' ? copyLinkIcon : copyLinkIconDark} 
+          alt="copyLinkIcon" 
+          className="h-11"/> 
+      </button>
     </section>
   );
 }
