61ac3c17aa0ecf21e17c7791729b06e651a9c3e8
Yoline <92892101+YolineB@users.noreply.github.com>
2023-02-28 10:42:20 -0800

    Add dark theme logo and logic

--- src/components/elements/CopyEventLink.jsx
+++ src/components/elements/CopyEventLink.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import copyLinkIcon from 'assets/ui/copyLinkIcon.svg'
-//Uncomment when dark mode logo is given
-// import copyLinkIconDark from 'assets/ui/copyLinkIcon.svg'
+import copyLinkIconDark from 'assets/ui/copyLinkIconDark.svg'
 
 function CopyEventLink() {
-  // const [colorTheme, setTheme] = useState(localStorage.theme);
-  // window.addEventListener("themeChanged", () => setTheme(localStorage.theme));
+  const [colorTheme, setTheme] = useState(localStorage.theme);
+  window.addEventListener("themeChanged", () => setTheme(localStorage.theme));
 
   return (
     <section className="flex flex-row gap-x-7 items-center text-xl dark:text-white font-medium">
       Copy Link
       <img 
-        // src={colorTheme === 'light' ? copyLinkIcon : copyLinkIconDark} 
-        src={copyLinkIcon} 
+        src={colorTheme === 'light' ? copyLinkIcon : copyLinkIconDark} 
         alt="copyLinkIcon" 
         className="h-11"/> 
     </section>
