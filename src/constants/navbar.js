885f8cb7a5d41a15fda673d20929e94431ee2c61
najwanan <66931010+najwanan@users.noreply.github.com>
2023-04-10 20:56:22 -0600

    adding navbar constants

--- src/constants/navbar.js
+++ src/constants/navbar.js
@@ -7,3 +7,17 @@ export const NAVBAR_EVENT_OPTIONS = [
 export const NAVBAR_SUPPORT_OPTIONS = [
   { label: 'Sponsors', value: 'sponsors', route: '/sponsors' },
 ]
+export const NAVBAR_PROFILE_OPTIONS = [
+  {
+    label: "Profile",
+    value: "profile",
+    route: "/settings",
+  },
+  {
+    label: "Sign Out",
+    value: "sign-out",
+    route: "/sign-in",
+    onclick: () => console.log("sign out complete"),
+    // const signOut = () => auth.signOutCurrentUser();
+  },
+];
